f725671a1c0f90d833750b26d275fad8819e98ff
nicsylvia <nicsylvia15f@gmail.com>
2023-02-08 12:48:27 -0800

    saved app, login and profile

---
+++ src/Components/ReduxState/Reduxstate.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const Reduxstate = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = Reduxstate.actions
+
+export default Reduxstate.reducer
