a5fcc1ad7b62340f699f3e9023e87a28ea332195
nicsylvia <nicsylvia15f@gmail.com>
2023-02-08 12:35:59 -0800

    saved app, login and profile

---
+++ src/Components/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <button>Login</button>
+        <button>Logout</button>
+    </div>
+  )
+}
+
+export default LoginPage
